b386b41198ee97195ac93ce236eca93080f83a48
Mani <manihoshiaripour83@gamil.com>
2026-05-09 23:26:23 +0330

    add seperator

--- calculator.js
+++ calculator.js
@@ -1,30 +1,83 @@
 const textinput = document.getElementById('textinput')
 const texta= document.getElementById('textpush')
-let input=''
+
 let a=0
 let b=0
 let operator=''
-
+textinput.textContent=''
+let value=''
 let percentboolean=false
+ document.addEventListener('keydown',(e)=>{
+if(e.key==='Escape'){
+    cleartext()
+}
+else if(e.key==="Backspace"){
+deletetext()
+}
+else if(e.key==='Enter'){
+equal()
+}else if(e.key==='%'){
+    percentnumber('%')
+}else if(('+-/*').includes(e.key)){
+    amaliat(e.key)
+}else if(('0123456789').includes(e.key))
+number(e.key)
+else if(e.key==='.'){
+    adddot(e.key)
+}
+ })
+
+ 
 
 function number(k) {
-    textinput.textContent += k
+     
+    value+=k
+   
+  let parts=value.split('.')
+  let int =parts[0]
+  let float=parts[1]
+  let result=''
+  let count=0
+  for (let i= int.length-1;i>=0; i--) {
+    result=int[i]+result
+    count++
+    if(count%3===0 && i!==0){
+    result=","+result
 
+  }
+  }
+  
+  if(float){
+    result+='.'+float
+  }
+    textinput.textContent  =result
+    
 }
+
 function cleartext() {
     textinput.textContent=''
+    value=''
+    texta.textContent=''
 
 }
+
+  function adddot(dot){
+    if(!(value.includes('.'))){
+    value+=dot
+    textinput.textContent=value
+    }
+}
+
+
 function deletetext(){
+   value=value.slice(0,-1)
    textinput.textContent=textinput.textContent.slice(0,-1)
 }
 function amaliat(amal){
-    a=textinput.textContent
-    a=parseFloat(a)
+    a=Number(value) 
     operator=amal
-    
-    textinput.textContent=''
-    textinput.textContent+=amal
+    value=''
+    textinput.textContent=amal
     texta.textContent=a
 }
 
@@ -32,43 +85,37 @@ function equal(){
     texta.textContent=''
     if(!percentboolean){
 
-    b=textinput.textContent.slice(1,10) 
+    // b=textinput.textContent.slice(1) 
     percentboolean=false
     }
-    b=parseFloat(b)
+    b=Number(value)
     
     if(operator==="+"){
-
       a+=b
-       textinput.textContent=a
     }
     if(operator==="-"){
         a=a-b
-        textinput.textContent=a
 
     }
     else if(operator=="*"){
-        
-        
         a=a*b
-        textinput.textContent=a
 
     }
     else if(operator=="/"){
  a=a/b
-        textinput.textContent=a
     }
-    
+    textinput.textContent=a.toLocaleString()
+    value=a.toString()
 }
 function percentnumber(percent){
  percentboolean=true
    textinput.textContent+=percent
    if(operator=='+'||operator=='-'){
-    textinput.textContent=a*(textinput.textContent.slice(1,-1)/100)
+    value=a*(textinput.textContent.slice(0,-1)/100)
     b=textinput.textContent
    }
    if(operator=='*'||operator=='/'){
-    textinput.textContent=textinput.textContent.slice(1,-1)/100   
+    value=textinput.textContent.slice(0,-1)/100   
     b=textinput.textContent
     
    }
